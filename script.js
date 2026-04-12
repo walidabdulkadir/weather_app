d63319c6bb39431979a8fc4b5d07a6819c44f069
walidabdulkadir <yesufabdulkadir@gmail.com>
2026-04-12 21:51:05 +0300

    add eventlistener

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-const weatherForm = document.getElementById("weather-form");
 const cityInput = document.getElementById("cityInput");
 const searchBtn = document.getElementById("searchBtn");
 const card = document.getElementById("card");
 const apiUrl = "c588a0f31ba23bb28b9dbd2bea5d73bc";
 
-weatherForm.addEventListener("submit", (e) => {
+searchBtn.addEventListener("click", async (e) => {
   e.preventDefault();
 
   const city = cityInput.value;
 
   if (city) {
     try {
-    } catch (error) {}
+      let weatherData = await getWeatherData(city);
+      displayWeatherInfo(weatherData);
+    } catch (error) {
+      displayError(error);
+    }
   } else {
-    displayError("Please, Enter a city");
+    displayError("Please, Enter a City");
   }
 });
 
-async function getWeatherData(city) {}
+async function getWeatherData(city) {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+}
 
 function displayWeatherInfo(data) {}
 
@@ -33,5 +39,5 @@ function displayError(message) {
   card.appendChild(errorDisplay);
   setTimeout(() => {
     card.textContent = "";
-  }, 9000);
+  }, 5000);
 }
