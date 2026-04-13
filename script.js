1b892df6317a35799760a02e949876c0f5bed56d
walidabdulkadir <yesufabdulkadir@gmail.com>
2026-04-13 11:49:19 +0300

    feat: implement weather data display logic, update API fetch parameters, and configure Live Server port

--- script.js
+++ script.js
@@ -21,19 +21,68 @@ searchBtn.addEventListener("click", async (e) => {
 });
 
 async function getWeatherData(city) {
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lat={lat}&lon={lon}&appid=${apiKey}`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   const response = await fetch(apiUrl);
 
   if (!response.ok) {
-    throw new error();
+    throw new Error();
   }
-  console.log(response);
+  // console.log(response);
   return await response.json();
 }
 
-function displayWeatherInfo(data) {}
+function displayWeatherInfo(data) {
+  const {
+    name: city,
+    main: { temp, humidity },
+    weather: [{ description, id }],
+  } = data;
 
-function getWeatherEmoji(weatherId) {}
+  card.textContent = "";
+  card.style.display = "flex";
+  card.style.flexDirection = "column";
+
+  const cityDis = document.createElement("h2");
+  const tempDis = document.createElement("h1");
+  const descriptionDis = document.createElement("p");
+  const humidityDis = document.createElement("p");
+  const emojiDis = document.createElement("div");
+
+  cityDis.classList.add("cityDis");
+  tempDis.classList.add("tempDis");
+  descriptionDis.classList.add("descriptionDis");
+  humidityDis.classList.add("humidityDis");
+  emojiDis.classList.add("emojiDis");
+
+  cityDis.textContent = city;
+  tempDis.textContent = temp.toFixed(2) + "°C";
+  descriptionDis.textContent = description;
+  humidityDis.textContent = "Humidity: " + humidity + "%";
+  emojiDis.textContent = getWeatherEmoji(id);
+
+  // append in order
+  card.append(cityDis, emojiDis, tempDis, descriptionDis, humidityDis);
+}
+
+function getWeatherEmoji(weatherId) {
+  if (weatherId >= 200 && weatherId < 300) {
+    return "⛈️"; // Thunderstorm
+  } else if (weatherId >= 300 && weatherId < 400) {
+    return "🌦️"; // Drizzle
+  } else if (weatherId >= 500 && weatherId < 600) {
+    return "🌧️"; // Rain
+  } else if (weatherId >= 600 && weatherId < 700) {
+    return "❄️"; // Snow
+  } else if (weatherId >= 700 && weatherId < 800) {
+    return "🌫️"; // Atmosphere (fog, mist, smoke)
+  } else if (weatherId === 800) {
+    return "☀️"; // Clear sky
+  } else if (weatherId > 800) {
+    return "☁️"; // Clouds
+  } else {
+    return "❓";
+  }
+}
 
 function displayError(message) {
   const errorDisplay = document.createElement("p");
