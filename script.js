fdff8c198feb514213a16dd1428720846ced44b7
walidabdulkadir <yesufabdulkadir@gmail.com>
2026-04-15 16:45:25 +0300

    refactor: reorganize event listener for search button and improve error handling

--- script.js
+++ script.js
@@ -3,23 +3,6 @@ const searchBtn = document.getElementById("searchBtn");
 const card = document.getElementById("card");
 const apiKey = "c588a0f31ba23bb28b9dbd2bea5d73bc";
 
-searchBtn.addEventListener("click", async (e) => {
-  e.preventDefault();
-
-  const city = cityInput.value;
-
-  if (city) {
-    try {
-      let weatherData = await getWeatherData(city);
-      displayWeatherInfo(weatherData);
-    } catch (error) {
-      displayError("Unknown Country");
-    }
-  } else {
-    displayError("Please, enter a country");
-  }
-});
-
 async function getWeatherData(city) {
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   const response = await fetch(apiUrl);
@@ -96,3 +79,20 @@ function displayError(message) {
     card.textContent = "";
   }, 5000);
 }
+
+searchBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  const city = cityInput.value;
+
+  if (city) {
+    try {
+      let weatherData = await getWeatherData(city);
+      displayWeatherInfo(weatherData);
+    } catch (error) {
+      displayError("Unknown Country");
+    }
+  } else {
+    displayError("Please, enter a country");
+  }
+});
